29fb1c4bfe56ff0397382488a363516527917677
james <jneeley@robinsage.com>
2019-10-31 23:37:36 -0600

    wire up the view with packageItemController methods, added onClick methods as well as download methods that handle state of getOrderedDataSet(). add new strings, and extension for showing dismiss alert

--- app/src/main/java/com/jamesneeley/packageinstaller/misc/Extensions.kt
+++ app/src/main/java/com/jamesneeley/packageinstaller/misc/Extensions.kt
@@ -1,2 +1,16 @@
 package com.jamesneeley.packageinstaller.misc
 
+import android.app.AlertDialog
+import android.content.Context
+import com.jamesneeley.packageinstaller.R
+
+fun Context.showDismissAlertWith(title: String, message: String) {
+    val dialogBuilder = AlertDialog.Builder(this)
+    dialogBuilder.setMessage(message)
+        .setCancelable(false)
+        .setNegativeButton(getString(R.string.dismiss)) { dialog, _ -> dialog.cancel() }
+
+    val alert = dialogBuilder.create()
+    alert.setTitle(title)
+    alert.show()
+}
